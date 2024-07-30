6b22e84827be013fa4584ededbd3738d930c078b
Gouri Sharma <150764295+Gsha36@users.noreply.github.com>
2024-07-30 21:45:59 +0530

    Update Result.js

--- src/Result.js
+++ src/Result.js
@@ -14,15 +14,29 @@ const Result = () => {
   const navigate = useNavigate();
   const { userChoice, compChoice, result, updatedScore } = location.state;
 
+  const userBorderColor = result === 'Win' ? 'green' : result === 'Lose' ? 'red' : 'orange';
+  const houseBorderColor = result === 'Lose' ? 'green' : result === 'Win' ? 'red' : 'orange';
+
   return (
     <div style={{height: '100vh', backgroundColor: '#192a56', color: 'white', textAlign: 'center', padding: '20px' }}>
-      <div style={{ padding: 40, fontSize: 40 }}>
+      <div style={{ 
+        margin: 20,
+        display: 'inline-block',
+        padding: '30px 20px', 
+        fontSize: 40,
+        borderRadius: '10px', 
+        border: '1px solid white'
+       }}>
         Rock, Paper, Scissors
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div style={{ margin: '0 20px' }}>
           <h2>You Picked</h2>
-          <img src={choices[userChoice.name]} alt={userChoice.name} style={{ height: 100, width: 100, backgroundColor: 'white', padding: 30, borderRadius: '50%' }} />
+          <img src={choices[userChoice.name]} alt={userChoice.name} style={{ 
+              height: 100, width: 100, backgroundColor: 'white',
+              padding: 30,
+              borderRadius: '50%',
+              border: `15px solid ${userBorderColor}`, }} />
         </div>
         <div style={{ margin: '0 20px' }}>
           <h2>{result}</h2>
@@ -30,7 +44,12 @@ const Result = () => {
         </div>
         <div style={{ margin: '0 20px' }}>
           <h2>The House Picked</h2>
-          <img src={choices[compChoice.name]} alt={compChoice.name} style={{ height: 100, width: 100, backgroundColor: 'white', padding: 30, borderRadius: '50%'}} />
+          <img src={choices[compChoice.name]} alt={compChoice.name} style={{ 
+            height: 100, width: 100, backgroundColor: 'white',
+              padding: 30,
+              borderRadius: '50%',
+              border: `15px solid ${houseBorderColor}`,
+          }} />
         </div>
       </div>
       <div style={{ padding: 20, fontSize: 20 }}>
